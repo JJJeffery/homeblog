8b5e107eeb1083ccaa40e7e5077fcd8d87adf391
heacheuy <heacheuy@live.cn>
2016-09-06 19:28:31 +0800

    delete other theme

--- wp-content/themes/Unite/index.php
+++
@@ -1,4 +0,0 @@
-<?php if (get_option('ygj_home') == '杂志布局') { ?>
-<?php include('cms.php'); ?>
-<?php } else if(get_option('ygj_home') == '博客导航') { include(TEMPLATEPATH . '/daohang.php'); ?>
-<?php } else { include(TEMPLATEPATH . '/blog.php'); } ?>
--- wp-content/themes/unik/portfolio-template-3columns.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Template Name: Portfolio 3 Columns */
-get_header(); 
-
-get_template_part('portfolio_loop');
-
-get_footer();
-?>
